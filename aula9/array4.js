c061c435a938c0c718debc68ab7bf6139824e14e
Cauã Israel <caua.israel02@gmail.com>
2025-02-07 16:56:48 -0300

    Update array4.js

--- aula9/array4.js
+++ aula9/array4.js
@@ -3,6 +3,5 @@ for(let i=0; i<3;i++) {
     let nomes = prompt(`Digite o ${i + 1}º nome: `);
     usuario.push(nomes);
 }
-let nomesA = nomes.sort(...usuario);
 
-console.log("Os nomes são: "+nomesA);
+console.log("Os nomes são: "+nomes);
